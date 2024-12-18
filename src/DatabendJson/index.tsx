417c5b69db6d3461a762faa4d5894321ce060e0b
Quan <787025321@qq.com>
2024-12-18 10:20:18 +0800

    fix: typo

--- src/DatabendJson/index.tsx
+++ src/DatabendJson/index.tsx
@@ -1,18 +1,18 @@
 // Copyright 2024 DatabendLabs.
-import { FC, ReactElement } from "react";
-import ReactJson, { ReactJsonViewProps } from "react-json-view";
+import React, { FC, ReactElement } from 'react';
+import ReactJson, { ReactJsonViewProps } from 'react-json-view';
 
 const DatabendJson: FC<ReactJsonViewProps> = ({ ...props }): ReactElement => {
   return (
     <ReactJson
       style={{
-        padding: "10px",
-        borderRadius: "4px",
+        padding: '10px',
+        borderRadius: '4px',
       }}
-      src={props?.src}
       displayDataTypes={false}
       theme="ashes"
       {...props}
+      src={props?.src}
     />
   );
 };
