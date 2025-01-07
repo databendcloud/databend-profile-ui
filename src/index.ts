67a01b4c779c296d7ac4f71f5e771dc80b09c3e7
Quan <787025321@qq.com>
2025-01-07 14:57:03 +0800

    fix: profile

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 export { default as QueryProfile } from './QueryProfile/index';
 export { default as TestProfile } from './QueryProfile/test-profile';
 export { default as DatabendJson } from './DatabendJson';
+// export { default as TaskGraph } from './TaskGraph';
